a082451337a7b0f1355dba28c0ca9866dad90a91
3laa Elsadany <alaaelsayed075544@gmail.com>
2025-04-24 13:16:02 +0200

    Setup passport-local login system

---
+++ routes/authRouter.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const {
+  isAuth
+} = require("../helpers/authentication")
+
+router.get("/", isAuth, (req, res, next) => {
+  res.render("dashboard", {
+    title: "Dashboard page",
+    isDashboard: true
+  })
+})
+
+module.exports = router;
